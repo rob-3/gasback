457a7bdea77c033fe0ec1f7e41c83e3210e6dbc3
Robert Boyd III <rob@robertcboyd.com>
2020-09-26 10:50:08 -0400

    Remove unnecessary code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { getCurrentLocation, calcDistance } from './locationFunctions';
 
@@ -11,7 +11,6 @@ export default function App() {
   //App State
   const [buttonText, setButtonText] = useState("Start");
   const [startLocation, setStartLocation] = useState(null);
-  const [endLocation, setEndLocation] = useState(null);
   const [distance, setDistance] = useState(null);
 
 
@@ -22,8 +21,7 @@ export default function App() {
     } else {
       setButtonText("Start");
       let end = await getCurrentLocation();
-      setEndLocation(end);
-      setDistance( await calcDistance(startLocation, end));
+      setDistance(await calcDistance(startLocation, end));
     }
   };
 
