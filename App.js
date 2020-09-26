598e1f9395311e7a36d448e8993428c3b0d16084
Taylor Bean <JacobBean18@gmail.com>
2020-09-25 23:52:42 -0400

    Added getCurrentLocation()

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ locationFunctions.js
@@ -0,0 +1,27 @@
+import * as Location from 'expo-location';
+
+
+export function getGeocode(destination){
+
+
+}
+
+
+export function calcDistance(destination){
+
+}
+
+export async function getCurrentLocation(){
+
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        //Display Location Permission Error
+        return null;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    
+    return [location.coords.latitude, location.coords.longitude];
+
+}
