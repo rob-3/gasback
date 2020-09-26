9a3bce166fc12f5de92542d674f997d9d592818c
Taylor Bean <JacobBean18@gmail.com>
2020-09-26 10:12:34 -0400

    Implemented the calcDistance & fetchDistanceData functions

--- locationFunctions.js
+++ locationFunctions.js
@@ -1,14 +1,21 @@
 import * as Location from 'expo-location';
 
-
+// This is where our environment vars are stored, including the API keys
+const dotenv = require('dotenv').config(`./config.env`);
+const distanceMatrixApi = `http://maps.googleapis.com/maps/api/distancematrix/json?units=`;
 export function getGeocode(destination){
 
 
 }
 
-
-export function calcDistance(currentLocation, destination){
-  return 5;
+// Uses the Google Distance Matrix API to calculate the distance between the 2 suplied location parameters
+// Both parameters are coord pairs
+export async function calcDistance(currentLocation, destination, units = `metric`){
+  const distanceJson = await fetchDistanceData(currentLocation, destination, units);
+  
+  //distanceData hold both a text property and a value property
+  distanceData = distanceJson.rows.elements.distance
+  return distanceData;
 }
 
 export async function getCurrentLocation(){
@@ -24,4 +31,12 @@ export async function getCurrentLocation(){
     
     return [location.coords.latitude, location.coords.longitude];
 
+}
+
+async function fetchDistanceData(currentLocation, destination, units = `metric`){
+  const responce = await fetch(`${distanceMatrixApi}${units}&origins=${currentLocation[0]},${currentLocation[1]}` + 
+    `&destinations=${destination[0]},${destination[1]}&key=${process.env.GOOGLE_API_KEY}`)
+    .then(r => r.json());
+
+    return responce;
 }
