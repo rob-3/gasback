998de91fc9b8b9515d2c3bb9579e7fbf9fd609e9
Taylor Bean <JacobBean18@gmail.com>
2020-09-26 23:59:48 -0400

    Fixed setItem() call, added mote detail to error handling

--- persistence.js
+++ persistence.js
@@ -2,18 +2,19 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 export async function storeData(data, id){
     try{
-        await AsyncStorage.setItem(`${id}`, make);
+        await AsyncStorage.setItem(`${id}`, data);
     }catch(e){
-        console.log(e);
+        console.log('storeData - ' + e);
     }
 }
 
 export async function getData(id){
     try{
         const ret = await AsyncStorage.getItem(`${id}`);
+        console.log(ret);
         return ret;
     }catch(e){
-        console.log(e);
+        console.log('getData - ' +e);
     }
 }
 
